0a34edc8d47268e3a4159877f7bcd52ce14e911a
Daniel Kwon <skansmfqh7312@gmail.com>
2022-07-18 15:28:56 +0900

    오타 수정

---
+++ TrendMedia/Practice/AttensionModeTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AttensionModeTableViewController.swift
+//  TrendMedia
+//
+//  Created by 권민서 on 2022/07/18.
+//
+
+import UIKit
+
+class AttensionModeTableViewController: UITableViewController {
+    
+    let modeSettingArray = 
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
