0bea0403af8f629cb74909cf4b7f8e9665892e9b
Daniel Kwon <skansmfqh7312@gmail.com>
2022-07-18 15:12:58 +0900

    설정 코딩 완료

--- TrendMedia/Practice/SettingTableViewController.swift
+++ TrendMedia/Practice/SettingTableViewController.swift
@@ -8,12 +8,77 @@
 import UIKit
 
 class SettingTableViewController: UITableViewController {
+    
+    var totalSettingArrary = ["공지사항", "실험실", "버전정보"]
+    var personalSettingArrary = ["개인/보안", "알림", "채팅", "멀티프로필"]
+    var etcArrary = ["고객센터/도움말"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
     }
+    
+    //섹션의 갯수 - 3
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    //헤더
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "전체 설정"
+        } else if section == 1 {
+            return "개인 설정"
+        } else if section == 2 {
+            return "기타"
+        }
+        return "오류"
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return ""
+    }
+    
+    //세션 갯수
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            // 첫번째 섹션의 Count(갯수)
+            return totalSettingArrary.count
+        } else if section == 1 {
+            // 두번쩨 섹션의 count( 갯수) Return 갯수 써도 무관
+            return personalSettingArrary.count
+        } else if section == 2 {
+            return etcArrary.count
+        }
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingCell")!
+        
+        if indexPath.section == 0 {
+            cell.textLabel?.text = totalSettingArrary[indexPath.row]
+            
+            // cell.textLable?.text = totalSettingArrary[0]
+            
+            cell.textLabel?.font = .boldSystemFont(ofSize: 15)
+            cell.textLabel?.textColor = .systemGray
+        } else if indexPath.section == 1 {
+            cell.textLabel?.text = personalSettingArrary[indexPath.row]
+            cell.textLabel?.textColor = .systemGray
+            cell.textLabel?.font = .boldSystemFont(ofSize: 15)
+        } else if indexPath.section == 2 {
+            cell.textLabel?.text = etcArrary[indexPath.row]
+            cell.textLabel?.textColor = .systemGray
+            cell.textLabel?.font = .boldSystemFont(ofSize: 15)
+        }
+        return cell 
+    }
+    
+    
 
   
 }
