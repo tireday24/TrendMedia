f5cead7e0a97b47a59df2fc97dd5fb1263d789a7
Daniel Kwon <skansmfqh7312@gmail.com>
2022-07-19 22:04:33 +0900

    contentView에 leading을 준 줄 알았는데 전체 View에 Constraints 했었음

---
+++ TrendMedia/UITextField+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UITextField+Extension.swift
+//  TrendMedia
+//
+//  Created by 권민서 on 2022/07/19.
+//
+
+import UIKit
+
+extension UITextField {
+    
+    func borderColor() {
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.borderWidth = 1.0
+        self.borderStyle = .none
+    }
+    
+    
+    
+}
