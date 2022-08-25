b2c0f548917e5bbe8de7771d6e8e92329a849b5b
Daniel Kwon <skansmfqh7312@gmail.com>
2022-08-25 12:16:50 +0900

    ui

---
+++ Shopping/Utility/UI/Color.swift
@@ -0,0 +1,18 @@
+//
+//  Color.swift
+//  TrendMedia
+//
+//  Created by 권민서 on 2022/08/24.
+//
+
+import UIKit
+
+enum Color {
+    enum BaseColor {
+        static let background = UIColor.white
+        static let border = UIColor.black.cgColor
+        static let text = UIColor.black
+        static let placeholder = UIColor.lightGray
+        static let point = UIColor.systemRed
+    }
+}
